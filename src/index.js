c1b33a7715ba3ee0319fa5a69d46b20c8389ebb6
Jonathan Santana <jonfesan@gmail.com>
2020-09-17 23:15:10 -0500

    clase6-ReactDom.render->Entendiendo_React

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
+//1-creando tipo de elemento
 // const element = document.createElement('h1');
+//2-que se coloca al elemento
 // element.innerText = 'Hello, Platzi Badges!';
 
+//contenedor del id en index.html dentro de body
 // const container = document.getElementById('app');
 
+//agregando el elemento al container
 // container.appendChild(element);
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//paso 1 y 2-> esto jsx  se debe importar react
 const element = <h1>Hello, Platzi Badges!</h1>;
 
 const container = document.getElementById('app');
 
-// ReactDOM.render(__qué__, __dónde__);
+// ReactDOM.render(__qué queremos renderizar__, __dónde lo queremos renderizar__);
 ReactDOM.render(element, container);
